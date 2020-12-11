98c07fd861aa5d53239bd47ef8f5e9521ebf6ed3
Len Huang <lendevelops@gmail.com>
2020-12-10 22:32:32 -0500

    set up navbar

--- site/src/App.js
+++ site/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, NavLink, Route } from "react-router-dom"
+import whitelotus from "./pictures/whitelotus.jpg"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Navbar bg="light">
+        <Nav className="mr-auto">
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img
+              alt=""
+              src={whitelotus}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+              ATLA: Regret Matching
+            </Navbar.Brand>
+          </NavLink>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/theory" >Theory</Nav.Link>
+            <Nav.Link href="/regret">Regret</Nav.Link>
+            <Nav.Link href="/code">Code</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch></Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
