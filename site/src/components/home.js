6d330fa37a37a81b28682f112753f920c6639cc0
Len Huang <lendevelops@gmail.com>
2020-12-11 02:36:58 -0500

    renderTable dynamic

--- site/src/components/home.js
+++ site/src/components/home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import Form from 'react-bootstrap/Form'
 import RenderTable from "./renderTable"
 
 const Title = () => {
@@ -12,9 +13,14 @@ const Title = () => {
 
 function Home() {
 
-    const RPS = [[[0, 0], [-1, 1], [1, -1]], [[1, -1], [0, 0], [-1, 1]], [[-1, 1], [1, -1], [0, 0]]]
-    const ATLA = [[[0, 0], [1, -1], [0, 0], [-1, 1]], [[-1, 1], [0, 0], [1, -1], [0, 0]], [[0, 0], [-1, 1], [0, 0], [1, -1]], [[1, -1], [0, 0], [-1, 1], [0, 0]]]
+    const RPS = "[[[0,0],[-1,1],[1,-1]],[[1,-1],[0,0],[-1,1]],[[-1,1],[1,-1],[0,0]]]";
+    const ATLA = "[[[0,0],[1,-1],[0,0],[-1,1]],[[-1,1],[0,0],[1,-1],[0,0]],[[0, 0],[-1, 1],[0, 0],[1,-1]],[[1,-1],[0,0],[-1,1],[0,0]]]"
     const [table, setTable] = useState(ATLA)
+
+    const handleChange = (e) => {
+        setTable(e.target.value)
+    }
+
     return (
     <div className="central-card">
         {Title()}
@@ -26,8 +32,27 @@ function Home() {
             more what this project was for in the <Link to="/about">about</Link> section.
         </div>
         <div className="content">
+            <h3> A Two Player Normal Form Game </h3>
+            <br />
             <RenderTable table={table} />
+            <br />
+            <Form>
+                <Form.Label> Write Out Your Matrix </Form.Label>
+                <Form.Control as="textarea" defaultValue={ATLA}
+                              rows={3} onChange={handleChange} />
+            </Form>
+            <br/>
+            Some examples of games you can try:
+            <ul>
+                <li>Prisoner's Dilemma: [[[-8,-8],[0,-10]],[[-10,0],[-1,-1]]]</li>
+                <li>Rock Paper Scissors: [[[0,0],[-1,1],[1,-1]],[[1,-1],[0,0],[-1,1]],[[-1,1],[1,-1],[0,0]]]</li>
+                <li>Aang vs Bang: [[[0,0],[1,-1],[0,0],[-1,1]],[[-1,1],[0,0],[1,-1],[0,0]],[[0, 0],[-1, 1],[0, 0],[1,-1]],[[1,-1],[0,0],[-1,1],[0,0]]]</li>
+            </ul>
         </div>
+        <div className="content">
+
+        </div>
+        <div className="empty"></div>
     </div>
   );
 }
--- site/src/components/renderTable.js
+++ site/src/components/renderTable.js
@@ -3,35 +3,41 @@ import Table from 'react-bootstrap/Table'
 
 // Takes in a 2D array and renders its elements
 const RenderTable = ({table}) => {
-    var matrix = []
-    if(table.length === 0){
-        return (
-            <div> Ill-formed Table</div>
-        )
-    } else {
-        var first = []
-        for(var i = 0; i < table[0].length; i++) {
-            first.push("B action " + String(i + 1))
+    try {
+        table = JSON.parse(table)
+        if(table.length === 0){
+            return (
+                <div> Ill-formed Table</div>
+            )
+        } else {
+            var first = []
+            for(var i = 0; i < table[0].length; i++) {
+                first.push("B action " + String(i + 1))
+            }
+            return (
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            {[<th key={"HeaderFirst"}></th>].concat(
+                                first.map((s,i) => <th key={"Header" + i}>{s}</th>)
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {table.map((row, i) =>
+                        <tr key={i}>
+                            {[<td key={"BodyFirst" + i}><b>A action {i + 1}</b></td>].concat(
+                                row.map((e,j) => <td key={"BodyRow" + i + "Col" + j}>{String(e)}</td>)
+                            )}
+                        </tr>
+                        )}
+                    </tbody>
+                </Table>
+            )
         }
+    } catch(err) {
         return (
-            <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        {[<th key={"HeaderFirst"}></th>].concat(
-                            first.map((s,i) => <th key={"Header" + i}>{s}</th>)
-                        )}
-                    </tr>
-                </thead>
-                <tbody>
-                {table.map((row, i) =>
-                    <tr key={i}>
-                        {[<td key={"BodyFirst" + i}><b>A action {i}</b></td>].concat(
-                            row.map((e,j) => <td key={"BodyRow" + i + "Col" + j}>{String(e)}</td>)
-                        )}
-                    </tr>
-                    )}
-                </tbody>
-            </Table>
+            <div style={{textAlign: "center"}}> Ill-formed Table :(</div>
         )
     }
 }
